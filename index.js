dd6117272bdf216aba560b7e6416bd7989e55896
Benjamin Carías <cariasbenjamin59@gmail.com>
2024-09-17 10:05:20 +0000

    Add LISTEN PORT

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));
 
 
+// Request Header Parser Challenge
 app.get('/api/whoami', function (req, res) {
 
     res.json({
@@ -13,4 +14,8 @@ app.get('/api/whoami', function (req, res) {
         "software": req.headers["user-agent"]
     });
 
+});
+
+app.listen(3000, () => {
+    console.log('App Listening in Port: ', 3000);
 })
